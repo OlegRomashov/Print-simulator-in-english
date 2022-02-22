282b1ed7061a6be500e6441670f875a4fa6a6132
OlegRomashov <dead_line@list.ru>
2022-02-22 12:50:15 +0300

    don	 work

--- src/js/replace.js
+++ src/js/replace.js
@@ -1,6 +1,16 @@
 function replace(str) {
   const findFor = [/[^a-zA-Z0-9а-яА-ЯёЁ.,!?:;’'"]/, /,+/,/\.+/, /!+/, /\?+/, / Mr. /, / Ms. /, /’/, /\s+/]
   const replaceWith = [' ', ',', '. ', '! ', '? ', ' Mr ',' Ms ', '\'', ' ']
+
+  if(str.includes('-->')) {
+    if(!str.includes('.')) {
+      findFor = [/[\d{1,3}\n\d{2}:\d{2}:\d{2},\d{3}\s-->\s\d{2}:\d{2}:\d{2},\d{3}\n]/,/[^a-zA-Z0-9а-яА-ЯёЁ.,!?:;’'"]/, /,+/,/\.+/, /!+/, /\?+/, / Mr. /, / Ms. /, /’/, /\s+/]
+      replaceWith = [' ',' ', ',', '. ', '! ', '? ', ' Mr ',' Ms ', '\'', ' ']
+    }
+    findFor = [/[]]/,/[^a-zA-Z0-9а-яА-ЯёЁ.,!?:;’'"]/, /,+/,/\.+/, /!+/, /\?+/, / Mr. /, / Ms. /, /’/, /\s+/]
+    replaceWith = [' ',' ', ',', '. ', '! ', '? ', ' Mr ',' Ms ', '\'', ' ']
+  }
+
   findFor.forEach( (i, j) => str = str.replace(new RegExp(i, "g"), replaceWith[j]) )
   return str
 }
